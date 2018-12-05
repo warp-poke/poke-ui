c13faa2adcc74c91a5157b48759ec0a8b2a14f7a
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2018-12-05 18:59:51 +0100

    Spectre.css customizing

--- scripts/change-base-color.js
+++ scripts/change-base-color.js
@@ -5,8 +5,10 @@ fs.readFile(someFile, 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  var result = data.replace(/\$primary-color: #5755d9 !default;/g, '$primary-color: #ff0066;');
-  var result = data.replace(/\$dark-color: #303742 !default;/g, '$dark-color: #800033;');
+  var result = 
+      data
+      .replace(/\$primary-color: #5755d9 !default;/g, '$primary-color: #ff0066;')
+      .replace(/\$dark-color: #303742 !default;/g, '$dark-color: #800033;');
 
   fs.writeFile(someFile, result, 'utf8', function (err) {
      if (err) return console.log(err);
