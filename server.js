7a9d8196a88ab1cea5e353c483f50bf81ea2fca6
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2018-12-05 18:19:45 +0100

    Deploying to Clever Cloud

---
+++ server.js
@@ -0,0 +1,28 @@
+/**
+ * Static HTTP Server
+ *
+ * Create a static file server instance to serve files
+ * and folder in the './public' folder
+ */
+
+// modules
+var static = require( 'node-static' ),
+    port = process.env.PORT || 8080,
+    http = require( 'http' );
+
+// config
+var file = new static.Server( './www', {
+    cache: 3600,
+    gzip: true
+} );
+
+// serve
+http.createServer( function ( request, response ) {
+    request.addListener( 'end', function () {
+        file.serve( request, response, (err, result) => { /*
+            if (err && (err.status === 404)) { // If the file wasn't found
+                file.serveFile('/index.html', 200, {}, request, response);
+            }        */
+        });
+    } ).resume();
+} ).listen( port );
